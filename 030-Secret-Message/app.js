6efbbae0c40e3c7ff4310ea01c508e7d61f836bd
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2023-02-20 12:56:38 +0000

    JavaScript Project - Arrays

---
+++ 030-Secret-Message/app.js
@@ -0,0 +1,12 @@
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+secretMessage.pop();
+console.log(secretMessage.length);
+secretMessage.push('to', 'Program');
+secretMessage[7] = 'right';
+secretMessage.shift();
+console.log(secretMessage);
+secretMessage.unshift('Programming');
+console.log(secretMessage);
+secretMessage.splice(6,5,'know');
+console.log(secretMessage);
+console.log(secretMessage.join(' '));
