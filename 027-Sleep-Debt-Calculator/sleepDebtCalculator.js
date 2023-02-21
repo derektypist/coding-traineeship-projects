aa507e16d018e75f54223dbef8e2bd512ed527b2
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2023-02-21 09:26:45 +0000

    JavaScript Project - Functions

--- 027-Sleep-Debt-Calculator/sleepDebtCalculator.js
+++ 027-Sleep-Debt-Calculator/sleepDebtCalculator.js
@@ -1,17 +1,49 @@
-const getActualSleepHours = () => 
-8 + 7 + 10 + 10 + 9 + 11 + 8;
+const getSleepHours = day => {
+  switch(day) {
+    case 'Monday':
+      return 9;
+      break;
+    case 'Tuesday':
+      return 9;
+      break;
+    case 'Wednesday':
+      return 9;
+      break;
+    case 'Thursday':
+      return 9;
+      break;
+    case 'Friday':
+      return 10;
+      break;
+    case 'Saturday':
+      return 9;
+      break;
+    case 'Sunday':
+      return 8;
+      break;
+    default:
+      return 'Error';
+    
+  }
+};
 
-const getIdealSleepHours = idealHours => idealHours * 7;
+const getActualSleepHours = () => 
+ getSleepHours('Monday') + getSleepHours('Tuesday') + getSleepHours('Wednesday') + getSleepHours('Thursday') + getSleepHours('Friday') + getSleepHours('Saturday') + getSleepHours('Sunday');
+
+const getIdealSleepHours = () => {
+  const idealHours = 9;
+  return idealHours * 7;
+}
 
 const calculateSleepDebt = () => {
   const actualSleepHours = getActualSleepHours();
-  const idealSleepHours = getIdealSleepHours(8);
+  const idealSleepHours = getIdealSleepHours();
   if (actualSleepHours === idealSleepHours) {
-    console.log('Your sleep hours this week are ' + actualSleepHours + ' and the ideal sleep hours this week are ' + idealSleepHours + '.  You got the perfect amount of sleep.')
+    console.log(`You have got the perfect amount of sleep.`);
   } else if (actualSleepHours > idealSleepHours) {
-    console.log('Your sleep hours this week are ' + actualSleepHours + ' and the total sleep hours this week are ' + idealSleepHours + '.  You got ' + (actualSleepHours - idealSleepHours) + ' hour(s) more sleep than you needed this week.');
+    console.log(`You have got ${actualSleepHours - idealSleepHours} hour(s) more sleep than needed.`);
   } else {
-    console.log('Your sleep hours this week are ' + actualSleepHours + ' and the ideal sleep hours this week are ' + idealSleepHours + '.  You got ' + (idealSleepHours - actualSleepHours) + ' hour(s) less sleep than you needed this week.  Get some rest.');
+    console.log(`You got ${idealSleepHours - actualSleepHours} hour(s) less sleep than you needed this week.  Get some rest.`);
   }
 }
 
