dc4fa4c8ba450fb7528037343b48ef2fba8de63c
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2023-02-23 15:35:04 +0000

    Added AddThoughtForm.js

---
+++ 051-Passing-Thoughts/AddThoughtForm.js
@@ -0,0 +1,35 @@
+import React, {useState} from 'react';
+import { generateId, getNewExpirationTime } from './utilities';
+
+export function AddThoughtForm(props) {
+  const [text,setText] = useState('');
+  const handleTextChange = (event) => {
+    setText(event.target.value);
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const thought = {
+      id: generateId(),
+      text: text,
+      expiresAt: getNewExpirationTime()
+    };
+
+  if (text.length > 0) {
+    props.addThought(thought);
+    setText('');
+  }
+    
+  }
+  return (
+    <form className="AddThoughtForm">
+      <input
+        type="text"
+        aria-label="What's on your mind?"
+        placeholder="What's on your mind?"
+        value={text}
+        onChange={handleTextChange}
+      />
+      <input type="submit" value="Add" onSubmit={handleSubmit}/>
+    </form>
+  );
+}
