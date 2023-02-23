0edc67bea52392ed72a86f7ec98e2865d708ef08
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2023-02-23 15:28:42 +0000

    Added App.js

---
+++ 051-Passing-Thoughts/App.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import { AddThoughtForm } from './AddThoughtForm';
+import { Thought } from './Thought';
+import { generateId, getNewExpirationTime } from './utilities';
+
+function App() {
+  const [thoughts, setThoughts] = useState([
+    {
+      id: generateId(),
+      text: 'This is a place for your passing thoughts.',
+      expiresAt: getNewExpirationTime(),
+    },
+    {
+      id: generateId(),
+      text: "They'll be removed after 15 seconds.",
+      expiresAt: getNewExpirationTime(),
+    },
+  ]);
+
+  const addThought = (thought) => {
+    setThoughts((thoughts) => [thought,...thoughts]);
+  };
+
+  const removeThought = (thoughtIdToRemove) => {
+    setThoughts((thoughts) => thoughts.filter((thought) => thought.id !== thoughtIdToRemove));
+  };
+
+  return (
+    <div className="App">
+      <header>
+        <h1>Passing Thoughts</h1>
+      </header>
+      <main>
+        <AddThoughtForm addThought={addThought}/>
+        <ul className="thoughts">
+          {thoughts.map((thought) => (
+            <Thought key={thought.id} thought={thought} removeThought={removeThought}/>
+          ))}
+        </ul>
+      </main>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
