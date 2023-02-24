c4b2a782295d98b0e8a4813ecae31d9ea07b716d
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2023-02-24 09:55:46 +0000

    Added searchTermSlice.js

---
+++ 057-Codecademy-Store/src/features/searchTerm/searchTermSlice.js
@@ -0,0 +1,20 @@
+/*
+Extra Credit:
+1. Create a function called searchTermReducer that can handle the following action types:
+    * 'searchTerm/setSearchTerm'
+    * 'searchTerm/clearSearchTerm'
+    * Don't forget to set the initial state and return state by default!
+
+2. Create a function called setSearchTerm
+    * It has one parameter, term
+    * It returns an action object whose payload is the term value
+    * See SearchTerm.js for how this will be used.
+
+3. Create a function called clearSearchTerm
+    * It returns an action object with no payload
+    * See SearchTerm.js for how this will be used.
+
+*/
+
+
+
