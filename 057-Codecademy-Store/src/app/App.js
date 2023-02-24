877105535cdef5712cb453271e89bdd0ddd64dae
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2023-02-24 09:40:48 +0000

    Added App.js

---
+++ 057-Codecademy-Store/src/app/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import { Inventory } from '../features/inventory/Inventory.js';
+import { CurrencyFilter } from '../features/currencyFilter/CurrencyFilter.js';
+// Import the Cart component here.
+import { Cart } from '../features/cart/Cart.js';
+// Render the Cart component below <Inventory />
+export const App = (props) => {
+
+  const { state, dispatch } = props;
+
+  return (
+    <div>
+      <CurrencyFilter
+        currencyFilter={state.currencyFilter}
+        dispatch={dispatch}
+      />
+
+      <Inventory
+        inventory={state.inventory}
+        currencyFilter={state.currencyFilter}
+        dispatch={dispatch}
+      />
+
+      <Cart 
+        cart={state.cart}
+        currencyFilter={state.currencyFilter}
+        dispatch={dispatch}
+        />
+
+    </div>
+  );
+};
