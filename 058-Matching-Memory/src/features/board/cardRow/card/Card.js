54c65bad1d13389d55645fb82dcd7ae97d67ea51
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2023-02-24 10:35:12 +0000

    Added Card.js

---
+++ 058-Matching-Memory/src/features/board/cardRow/card/Card.js
@@ -0,0 +1,52 @@
+import React from 'react';
+// Add import statements below
+import { useSelector, useDispatch } from 'react-redux';
+import { selectVisibleIDs, flipCard, selectMatchedIDs } from '../../boardSlice.js';
+
+let cardLogo = "https://static-assets.codecademy.com/Courses/Learn-Redux/matching-game/codecademy_logo.png";
+
+export const Card = ({ id, contents }) => {
+  // Add selected data and dispatch variables below
+  const visibleIDs = useSelector(selectVisibleIDs);
+  const dispatch = useDispatch();
+  const matchedIDs = useSelector(selectMatchedIDs);
+  
+  // flip card action
+  const flipHandler = (id) => {
+    // Add action dispatch below
+    dispatch(flipCard(id));
+  };
+
+  let cardStyle = 'resting'
+  let click = () => flipHandler(id);
+  
+  let cardText = (
+    <img src={cardLogo} className="logo-placeholder" alt="Card option" />
+  );
+
+  // 1st if statement
+  // implement card id array membership check
+  if (visibleIDs.includes(id) || matchedIDs.includes(id)) {
+    cardText = contents;
+    click = () => {};
+    
+  }
+
+  // 2nd if statement
+  // implement card id array membership check
+  if (matchedIDs.includes(id)) {
+    cardStyle = 'matched';
+  }
+
+  // 3rd if statement
+  // implement number of flipped cards check
+  if (visibleIDs.length === 2) {
+    click = () => {};
+  }
+
+  return (
+    <button onClick={click} className={`card ${cardStyle}`}>
+      {cardText}
+    </button>
+  );
+};
