920358e006b7a3ac95456fa8002e5eb3d8267bb1
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2023-02-24 09:39:05 +0000

    Added index.js

---
+++ 057-Codecademy-Store/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { App } from './app/App.js';
+// Import the store here.
+import { store } from './app/store.js';
+
+// Pass state and dispatch props to the <App /> component.
+const render = () => {
+  ReactDOM.render(
+    <App 
+      state={store.getState()}
+      dispatch={store.dispatch}
+    />,
+    document.getElementById('root')
+  )
+};
+render();
+
+// Subscribe render to the store.
+store.subscribe(render);
