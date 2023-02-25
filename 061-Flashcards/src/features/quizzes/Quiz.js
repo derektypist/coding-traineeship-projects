8adea30c33cc4eef61c2c2a482db6549be8d08cb
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2023-02-25 12:45:47 +0000

    Added Quiz.js

---
+++ 061-Flashcards/src/features/quizzes/Quiz.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link, useParams } from "react-router-dom";
+import Card from "../cards/Card";
+import ROUTES from "../../app/routes";
+import { useSelector } from "react-redux";
+import { selectQuizzes } from "./quizzesSlice";
+
+export default function Topic() {
+  const quizzes = useSelector(selectQuizzes); // replace this with a call to your selector to get all the quizzes in state
+  let { quizId } = useParams();
+  const quiz = quizzes[quizId];
+
+  return (
+    <section>
+      <h1>{quiz.name}</h1>
+      <ul className="cards-list">
+        {quiz.cardIds.map((id) => (
+          <Card key={id} id={id} />
+        ))}
+      </ul>
+      <Link to={ROUTES.newQuizRoute()} className="button center">
+        Create a New Quiz
+      </Link>
+    </section>
+  );
+}
