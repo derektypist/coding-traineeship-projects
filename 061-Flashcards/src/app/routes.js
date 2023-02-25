d0333ed0c33bbb196f810a7e38bd75603e004276
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2023-02-25 12:37:14 +0000

    Added routes.js

---
+++ 061-Flashcards/src/app/routes.js
@@ -0,0 +1,11 @@
+const ROUTES = {
+    newQuizRoute: () => "/quizzes/new",
+    quizRoute: (id) => `/quizzes/${id}`,
+    quizzesRoute: () => "/quizzes",
+    newTopicRoute: () => "/topics/new",
+    topicRoute: (id) => `/topics/${id}`,
+    topicsRoute: () => "/topics",
+  };
+  
+  export default ROUTES;
+  
