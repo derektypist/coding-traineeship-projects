190752f85d05b0f20ff42f595f771edb4d5dca53
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2023-03-06 16:20:40 +0000

    Added server.js

---
+++ 065-Quote-API/server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+
+const { quotes } = require("./data");
+const { getRandomElement } = require("./utils");
+
+const PORT = process.env.PORT || 4001;
+
+app.use(express.static("public"));
+
+app.get("/api/quotes/random", (req, res, next) => {
+  const quote = getRandomElement(quotes);
+  res.send({ quote: quote });
+});
+
+app.get("/api/quotes", (req, res, next) => {
+  if (!req.query.hasOwnProperty("person")) {
+    res.send({ quotes: quotes });
+  } else {
+    const filterQuote = quotes.filter(
+      (element) => element.person === req.query.person
+    );
+    res.send({ quotes: filterQuote });
+  }
+});
+
+app.post("/api/quotes", (req, res, next) => {
+  if (req.query.quote && req.query.person) {
+    const theNewQuote = { quote: req.query.quote, person: req.query.person };
+    quotes.push(theNewQuote);
+    res.send({ quote: theNewQuote });
+  } else {
+    res.status(400).send();
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server Quote API Listening on PORT ${PORT}.`);
+});
