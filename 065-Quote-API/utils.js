a11fe54ef1a3bd4340a5eea1a3b1365ce1ce60d1
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2023-03-06 16:21:46 +0000

    Updated utils.js

---
+++ 065-Quote-API/utils.js
@@ -0,0 +1,9 @@
+const getRandomElement = arr => {
+    if (!Array.isArray(arr)) throw new Error('Expected an array');
+    return arr[Math.floor(Math.random() * arr.length)];
+  }
+  
+  module.exports = {
+    getRandomElement
+  };
+  
