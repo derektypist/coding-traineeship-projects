dc9b36bf32a301094a51008f604f9f1d8ac38ad6
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2023-03-06 11:59:45 +0000

    Added Header.js

---
+++ 062-Reddit-Client/features/Header/Header.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './Header.css';
+import ThemeSwitch from '../Theme/ThemeSwitch';
+import Search from '../Search/Search';
+import { FaReddit } from 'react-icons/fa';
+import { AiFillGithub } from 'react-icons/ai';
+
+const Header = () => {
+    return (
+        <header>
+            <div id="header-main-half">
+                <a href="/">
+                    <div className="logo-container">
+                        <FaReddit className='logo' />
+                        <h1>Reddit</h1>
+                    </div>
+                </a>
+                <Search className="search-component" />
+            </div>
+
+            <div id="theme-switch">
+                <ThemeSwitch />
+                <a href="https://github.com/derektypist/reddit-client" className="github-link">
+                    <AiFillGithub />
+                </a>
+            </div>
+        </header>
+    )
+};
+
+export default Header;
