c535d351e1f01f0969da767d38a6c9351586a2a1
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2023-03-06 10:55:07 +0000

    Added App.js

---
+++ 062-Reddit-Client/app/App.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "../features/Theme/theme.css";
+import "./App.css";
+import Header from "../features/Header/Header";
+import Home from "../features/Home/Home";
+import Subreddit from "../features/Subreddit/Subreddit";
+import SearchResults from "../features/SearchResults/SearchResults";
+import SubredditsAside from "../features/SubredditsAside/SubredditsAside";
+import NotFoundPage from "../features/NotFoundPage/NotFoundPage";
+import { useSelector } from "react-redux";
+import { selectDarkMode } from "../features/Theme/themeSlice";
+
+function App() {
+
+  const darkMode = useSelector(selectDarkMode);
+
+  return (
+    <Router>
+      <div id="app-container" className={darkMode ? "dark" : "light"}>
+        <Header />
+        <main>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/r/:id" component={Subreddit} />
+            <Route path="/search/:id" component={SearchResults} />
+            <Route component={NotFoundPage} />
+          </Switch>
+        </main>
+        <aside>
+          <SubredditsAside />
+        </aside>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
