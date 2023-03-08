abff0e194a933ead402b7bf8f9d01ea6832acc29
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2023-03-08 16:22:02 +0000

    Added index-test.js

---
+++ 068-Quote-Keeper-Server-Layer/test/routes/index-test.js
@@ -0,0 +1,40 @@
+// test/routes/index-test.js
+const {assert} = require('chai');
+const request = require('supertest');
+const {jsdom} = require('jsdom');
+
+const app = require('../../app');
+
+const parseTextFromHTML = (htmlAsString, selector) => {
+    const selectedElement = jsdom(htmlAsString).querySelector(selector);
+    if (selectedElement !== null) {
+      return selectedElement.textContent;
+    } else {
+      throw new Error(`No element with selector ${selector} found in HTML string`);
+    }
+};
+
+
+describe('/', () => {
+  describe('POST', () => {
+    it('creates a new quote', async () => {
+      const quote = "Our deepest fear is not that we are inadequate. Our deepest fear is that we are powerful beyond measure.";
+      const attributed = 'Marianne Williamson';
+      const source = 'A Return to Love: Reflections on the Principles of A Course in Miracles';
+
+      const response = await request(app)
+      .post('/')
+      .type('form')
+      .send({quote, attributed, source});
+
+      assert.equal(response.status, 200);
+      assert.include(parseTextFromHTML(response.text, '#quotes'), quote);
+      assert.include(parseTextFromHTML(response.text, '#quotes'), attributed);
+      assert.include(parseTextFromHTML(response.text, '#quotes'), source);
+
+
+
+
+    });
+  });
+});
