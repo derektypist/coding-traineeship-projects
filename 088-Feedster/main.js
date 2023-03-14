bed03269be91698ff19e4ecb37c204b74e03eba1
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2023-03-14 10:56:28 +0000

    jQuery Project - Event Handlers

---
+++ 088-Feedster/main.js
@@ -0,0 +1,28 @@
+$(document).ready(() => {
+    $('.menu').on('mouseenter', () => {
+      $('.nav-menu').show();
+    }).on('mouseleave', () => {
+      $('.nav-menu').hide();
+    })
+  
+    $('.btn').on('mouseenter', (event) => {
+      $(event.currentTarget).addClass('btn-hover').on('mouseleave', (event) => { $(event.currentTarget).removeClass('btn-hover');
+      });
+    })
+    
+    $('.postText').on('keyup', (event) => {
+      let post = $(event.currentTarget).val();
+      let remaining = 140 - post.length;
+      if (remaining <=0) {
+        $('.wordcount').addClass('red');
+      } else {
+        $('.wordcount').removeClass('red');
+      }
+      $('.characters').html(remaining);
+    });
+  
+    $('.postText').focus();
+  
+    
+  }); 
+  
