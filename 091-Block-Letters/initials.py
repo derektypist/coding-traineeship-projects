53b4d4ac7f7f47b720e6e7f3f607df240e3dbecd
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2023-03-14 12:11:53 +0000

    Python Project - Hello World

---
+++ 091-Block-Letters/initials.py
@@ -0,0 +1,9 @@
+# Derek Dhammaloka
+# Had crushes with Kathy Lette and Rosana Franco
+print("DDDD  DDDD ")
+print("D   D D   D")
+print("D   D D   D")
+print("D   D D   D")
+print("D   D D   D")
+print("D   D D   D")
+print("DDDD  DDDD ")
