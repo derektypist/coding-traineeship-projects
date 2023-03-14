2461609e849537c82ef50b8734b37a244f105c1a
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2023-03-14 10:37:38 +0000

    jQuery Project - Introduction

---
+++ 086-Madison-Square-Market/main.js
@@ -0,0 +1,19 @@
+$(document).ready(() => {
+    $('#cart').on('click', () => {
+      $('#cartMenu').show();
+    })
+  
+    $('#account').on('click', () => {
+      $('#accountMenu').show();
+    })
+  
+    $('#help').on('click', () => {
+      $('#helpMenu').show();
+    })
+  
+    $('.dropdown-menu').on('mouseleave', () => {
+      $('.dropdown-menu').hide();
+    })
+  
+  
+  });
