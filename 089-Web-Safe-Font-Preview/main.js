ce3d77898539b42d548563c1c886f0d230a6da81
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2023-03-14 11:31:25 +0000

    jQuery Project - Style Methods

---
+++ 089-Web-Safe-Font-Preview/main.js
@@ -0,0 +1,15 @@
+$(document).ready(() => {
+    $('#text').on('keyup', (event) => {
+      $('.preview').html($(event.currentTarget).val());
+      $('#font').on('change', (event) => {
+        $('.preview').css({fontFamily: $(event.currentTarget).val()});
+      });
+      $('#weight').on('change', (event) => {
+        $('.preview').css({fontWeight: $(event.currentTarget).val()});
+      });
+      $('#size').on('keyup', (event) => {
+        let fontSize = $(event.currentTarget).val() + 'px';
+        $('.preview').css({fontSize: fontSize});
+      });
+    });
+  })
