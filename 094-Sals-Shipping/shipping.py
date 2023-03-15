35081789a087db3b576d1a7e9e8152777ce784f8
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2023-03-15 17:46:34 +0000

    Python Project - Control Flow with Functions

--- 094-Sals-Shipping/shipping.py
+++ 094-Sals-Shipping/shipping.py
@@ -1,31 +1,48 @@
-weight = 80
+# Function to Calculate Ground Shipping
+def groundshipping(weight):
+    if weight <=2:
+        cost_ground = (weight * 1.5) + 20
+    elif weight <=6:
+        cost_ground = (weight * 3) + 20
+    elif weight <=10:
+        cost_ground = (weight * 4) + 20
+    else:
+        cost_ground = (weight * 4.75) + 20
+    return cost_ground
 
-# Ground Shipping
-if weight <=2:
-    cost_ground = (weight * 1.5) + 20
-elif weight <=6:
-    cost_ground = (weight * 3) + 20
-elif weight <=10:
-    cost_ground = (weight * 4) + 20
-else:
-    cost_ground = (weight * 4.75) + 20
-
-print("Ground Shipping: ${0:.2f}".format(cost_ground))
+# Calculate Ground Shipping with Weight of 8.4 pounds
+print("Ground Shipping: ${0:.2f}".format(groundshipping(8.4)))
 
 # Ground Shipping Premium
 cost_ground_premium = 125.00
 
 print("Ground Shipping Premium: ${0:.2f}".format(cost_ground_premium))
 
-# Drone Shipping
+# Function to Calculate Drone Shipping
 
-if weight <=2:
-    cost_drone = (4.5 * weight)
-elif weight <=6:
-    cost_drone = (9.00 * weight)
-elif weight <=10:
-    cost_drone = (12.00 * weight)
-else:
-    cost_drone = (14.25 * weight)
+def droneshipping(weight):
+    if weight <=2:
+        cost_drone = (4.5 * weight)
+    elif weight <=6:
+        cost_drone = (9.00 * weight)
+    elif weight <=10:
+        cost_drone = (12.00 * weight)
+    else:
+        cost_drone = (14.25 * weight)
+    return cost_drone
 
-print("Drone Shipping: ${0:.2f}".format(cost_drone))
+# Calculate Drone Shipping with Weight of 1.5 pounds
+print("Drone Shipping: ${0:.2f}".format(droneshipping(1.5)))
+
+# Apply Two Examples
+test1 = 4.8
+test2 = 41.5
+print("Data with {0} pounds".format(test1))
+print("Ground Shipping $ {0:.2f}".format(groundshipping(test1)))
+print("Ground Shipping Premium $ {0:.2f}".format(cost_ground_premium))
+print("Drone Shipping $ {0:.2f}".format(droneshipping(test1)))
+
+print("\nData with {0} pounds".format(test2))
+print("Ground Shipping $ {0:.2f}".format(groundshipping(test2)))
+print("Ground Shipping Premium $ {0:.2f}".format(cost_ground_premium))
+print("Drone Shipping $ {0:.2f}".format(droneshipping(test2)))
