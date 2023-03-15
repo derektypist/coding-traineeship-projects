4b43bd9f79d3a8147e7d9d6d5323abc4820e19ca
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2023-03-15 17:32:34 +0000

    Python Project - Control Flow 2

--- 094-Sals-Shipping/shipping.py
+++ 094-Sals-Shipping/shipping.py
@@ -16,3 +16,16 @@ print("Ground Shipping: ${0:.2f}".format(cost_ground))
 cost_ground_premium = 125.00
 
 print("Ground Shipping Premium: ${0:.2f}".format(cost_ground_premium))
+
+# Drone Shipping
+
+if weight <=2:
+    cost_drone = (4.5 * weight)
+elif weight <=6:
+    cost_drone = (9.00 * weight)
+elif weight <=10:
+    cost_drone = (12.00 * weight)
+else:
+    cost_drone = (14.25 * weight)
+
+print("Drone Shipping: ${0:.2f}".format(cost_drone))
