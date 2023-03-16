7f2be88823138b988f033f46900bfec9e74b3de0
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2023-03-16 11:47:36 +0000

    Added index.php

---
+++ 107-HTML-PHP-Calculator/index.php
@@ -0,0 +1,20 @@
+<html>
+<body>
+<!--Your code goes here-->
+<h3>Addition</h3>
+<form method="get" action="addition.php">
+First Number: <input type="number" name="add_first"><br>
+Second Number: <input type="number" name="add_second"><br>
+<button type="submit">Add</button>
+<br>
+</form>
+<h3>Division</h3>
+<form method="get" action="division.php">
+Numerator: <input type="number" name="div_num"><br>
+Denominator: <input type="number" name="div_den"><br>
+<button type="submit">Divide</button>
+<br>
+</form>
+<a href="index.php">Reset</a>
+</body>
+</html>
