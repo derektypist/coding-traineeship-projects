d0fbfe2a502972c36431a68470ebf664a0d189b4
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2023-03-16 11:50:07 +0000

    PHP Project - Form Handling

---
+++ 107-HTML-PHP-Calculator/division.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+<!--Your code goes here-->
+<?="${_GET['div_num']} divided by ${_GET['div_den']} is:"?>
+<h4>
+<?=$_GET['div_num']/$_GET['div_den']?>
+</h4>
+<a href="index.php">Reset</a>
+</body>
+</html>
