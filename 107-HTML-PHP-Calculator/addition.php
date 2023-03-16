480118dbc59abdadf3ac54e8bef4e31b9ee57e68
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2023-03-16 11:48:55 +0000

    Added addition.php

---
+++ 107-HTML-PHP-Calculator/addition.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+<!--Your code goes here-->
+<?="The sum of ${_GET['add_first']} and ${_GET['add_second']} is:"?>
+<h4>
+<?=$_GET['add_first']+$_GET['add_second']?>
+</h4>
+<a href="index.php">Reset</a>
+</body>
+</html>
