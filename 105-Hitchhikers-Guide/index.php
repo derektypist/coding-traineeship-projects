d7bd4da433679a07281207f0423a881847f4cc01
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2023-03-16 11:38:19 +0000

    PHP Project - Built-In Functions

---
+++ 105-Hitchhikers-Guide/index.php
@@ -0,0 +1,22 @@
+<?php
+$initial = '555';
+$a = octdec($initial);
+echo "$a \n";
+$b = deg2rad($a);
+echo "$b \n";
+$c = cos($b);
+echo "$c \n";
+$d = round($c,3);
+echo "$d \n";
+$e = log($d);
+echo "$e \n";
+$f = abs($e);
+echo "$f \n";
+$g = acos($f);
+echo "$g \n";
+$h = rad2deg($g);
+echo "$h \n";
+$i = floor($h);
+echo "$i \n";
+$j = $i - 47;
+echo "$j \n";
